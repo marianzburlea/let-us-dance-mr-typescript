de78ec348c3a211cdf6c43811a202ee7af921c37
Marian Zburlea <marianzburlea@gmail.com>
2019-04-15 07:11:31 +0100

    Let's rock Mr. TypeScript. The dance ring is on

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Delays the execution by n miliseconds
+ * @param n number
+ */
+const timeout = (n: number) => new Promise(response => setTimeout(response, n))
+
+/**
+ * Adds numbers together
+ * @param a number
+ * @param b number
+ */
+export const sumNumber = async (a: number, b: number) => {
+  await timeout(500)
+  return a + b
+}
+
+// run me
+(async () => {
+  console.log(sumNumber(5, 2))
+})()
