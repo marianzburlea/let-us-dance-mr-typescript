0bcc92e9f4e33f090659e368212289c41998824d
Marian Zburlea <marianzburlea@gmail.com>
2019-05-02 23:53:07 +0100

    arrays in interface

--- let-us-do-it/03-we-are-sweating.ts
+++ let-us-do-it/03-we-are-sweating.ts
@@ -1,4 +1,19 @@
-import { HasGreenCar } from "./01-first-steps";
+import { HasGreenCar, HasAddress } from "./01-first-steps";
+
+interface MobileNumber {
+  [prop:string]: undefined | {
+    number: number,
+    code: number
+  }
+}
+
+const samsung: MobileNumber = {}
+if (typeof samsung.num === 'string') {
+  samsung.num = {
+    number: 123,
+    code: 2
+  }
+}
 
 type NumberOrBoolean = number | boolean;
 type foodProvisions = { hasFood: boolean };
@@ -6,3 +21,12 @@ type foodProvisions = { hasFood: boolean };
 export interface HasCountry extends HasGreenCar {
   country: string;
 }
+
+interface ContactSomeone {
+  (message: HasGreenCar | HasAddress, note: string): void;
+}
+
+type ContactSomeoneType = (
+  message: HasGreenCar | HasAddress,
+  note: string
+) => void
