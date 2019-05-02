247a7b12b11ce5862f6e051b8cd190af4ae3e3b3
Marian Zburlea <marianzburlea@gmail.com>
2019-05-02 20:53:46 +0100

    compile to es8

--- test-ts/src/index.js
+++ test-ts/src/index.js
@@ -1,11 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 /* global Promise */
 /**
  * Delays the execution by n miliseconds
@@ -17,11 +9,11 @@ const timeout = (n) => new Promise(response => setTimeout(response, n));
  * @param a number
  * @param b number
  */
-export const sumNumber = (a, b) => __awaiter(this, void 0, void 0, function* () {
-    yield timeout(500);
+export const sumNumber = async (a, b) => {
+    await timeout(500);
     return a + b;
-});
+};
 // run me
-(() => __awaiter(this, void 0, void 0, function* () {
+(async () => {
     console.log(sumNumber(5, 2));
-}))();
+})();
