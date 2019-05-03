9149d29d883415e7b34c0cbc918ae7f3d2b44588
Marian Zburlea <marianzburlea@gmail.com>
2019-05-03 10:10:46 +0100

    Create abstract class. Note that it must contain a super call in the constructor

--- let-us-do-it/04-classy.ts
+++ let-us-do-it/04-classy.ts
@@ -1,4 +1,29 @@
 import { HasCountry } from "./03-we-are-sweating";
+import { HasGreenCar } from "./01-first-steps";
+
+abstract class ExtendMeAbstract implements HasGreenCar, HasCountry {
+  abstract carIsTeleported: boolean
+  constructor(
+    public name: string,
+    public car: string,
+    public country: string
+  ) {}
+
+  abstract teleportCar():void
+}
+
+class NonAbstract extends ExtendMeAbstract {
+  public carIsTeleported: boolean = false
+  constructor(
+    public name: string,
+    public car: string,
+    public country: string
+  ) {
+    super(name, car, country)
+  }
+
+  teleportCar() {}
+}
 
 class GigiKent implements HasCountry {
   age: number = 37
